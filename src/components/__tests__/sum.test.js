2f0217dd4b3e2bf03ea1c9026101099ff29e3978
shreya kumari <shreypilot28@gmail.com>
2023-02-19 20:52:51 +0530

    Testing

---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,8 @@
+import {sum} from "../sum"
+
+
+
+test("check sum of 2 positive numbers", () =>{//every test cases should expect something
+//it is assertion
+   expect(sum(2,5).toBe(5));
+})
---
+++ src/components/sum.js
@@ -0,0 +1,35 @@
+export const sum = (a, b) => a + b;
+
+/**
+ *
+ *  Different Type of Testing??
+ *  - Manual Testing//by human
+ *  - Autmation Testing//by code testing code
+ *      - Selenium Testing
+ *
+ *
+ *  - E2E Testing - Covers entire user Journey//end to end
+ *
+ *  - Unit Testing//small component feature testing
+ *  - Integration Testing
+ *
+ *
+ *
+ *
+ * - Install React Testing Librabry
+ * - Install Jest
+ * - configure Jest//npx jest --init
+ * 
+ * - Installed jest-environment-jsdom
+ * - Create my first test
+ * - Configure babel
+ * - Wrote expect sum test
+ * - gitignore coverage report
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
